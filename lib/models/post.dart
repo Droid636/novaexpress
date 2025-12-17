0a04fff77e1780625a5c848e57d0e67ccb7860c2
Droid636 <2022043@utsh.edu.mx>
2025-12-16 20:21:54 -0600

    consumir

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -16,5 +16,28 @@ class Post with _$Post {
     @Default('') String date,
   }) = _Post;
 
-  factory Post.fromJson(Map<String, dynamic> json) => _$PostFromJson(json);
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'] as int,
+      title:
+          (json['title'] is Map ? json['title']['rendered'] : json['title']) ??
+          '',
+      content:
+          (json['content'] is Map
+              ? json['content']['rendered']
+              : json['content']) ??
+          '',
+      link: json['link'] as String? ?? '',
+      excerpt: json['excerpt'] is Map
+          ? (json['excerpt']['rendered'] ?? '')
+          : (json['excerpt'] ?? ''),
+      featuredImage: json['jetpack_featured_media_url'] as String? ?? '',
+      categories:
+          (json['categories'] as List<dynamic>?)
+              ?.map((e) => e as int)
+              .toList() ??
+          [],
+      date: json['date'] as String? ?? '',
+    );
+  }
 }
