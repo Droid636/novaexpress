99d4a96bbbb10d5e675f9be9b575e8ff03a08d3f
Droid636 <2022043@utsh.edu.mx>
2025-12-17 11:27:16 -0600

    cache

--- lib/services/favorites_cache_service.dart
+++ lib/services/favorites_cache_service.dart
@@ -5,6 +5,7 @@ import '../models/post_adapter.dart';
 
 class FavoritesCacheService {
   static const String _boxName = 'favorites_cache';
+  static const String _idsBoxName = 'favorites_ids_cache';
 
   static Future<void> init() async {
     await Hive.initFlutter();
@@ -12,6 +13,7 @@ class FavoritesCacheService {
       Hive.registerAdapter(PostAdapter());
     }
     await Hive.openBox<Post>(_boxName);
+    await Hive.openBox<int>(_idsBoxName);
   }
 
   static Future<void> saveFavorites(List<Post> posts) async {
@@ -25,5 +27,18 @@ class FavoritesCacheService {
   static List<Post> getFavorites() {
     final box = Hive.box<Post>(_boxName);
     return box.values.toList();
+  }
+
+  static Future<void> saveFavoriteIds(Set<int> ids) async {
+    final box = Hive.box<int>(_idsBoxName);
+    await box.clear();
+    for (var id in ids) {
+      await box.put(id, id);
+    }
+  }
+
+  static Set<int> getFavoriteIds() {
+    final box = Hive.box<int>(_idsBoxName);
+    return box.values.toSet();
   }
 }
